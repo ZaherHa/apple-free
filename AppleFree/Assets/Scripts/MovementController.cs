c8bf4c0159f42ca4da3e14eb6b2b3a121c8f89da
Tencho Tenev <tenev.dev@outlook.com>
2017-03-18 21:11:01 +0000

    Create apple and follow with camera

---
+++ AppleFree/Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    private Rigidbody body;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(h, 0, v);
+
+        body.AddForce(Time.deltaTime * speed * movementVector);
+    }
+}
