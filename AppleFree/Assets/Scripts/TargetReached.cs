b570883e490d63d06a687966ee6cbb59017897db
Tencho Tenev <tenev.dev@outlook.com>
2017-03-18 22:56:14 +0000

    Add script which detects when apple reaches an object

---
+++ AppleFree/Assets/Scripts/TargetReached.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetReached : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("You win");
+        }
+    }
+}
